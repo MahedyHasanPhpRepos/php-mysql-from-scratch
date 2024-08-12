e3216558e1370de682a7d15281a38ef6b0e73f46
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-08-12 17:55:13 +0600

    creating table inside our db

--- db.php
+++ db.php
@@ -9,20 +9,24 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // $sql_command = "create database TestDb" ; 
-    // $conn->exec($sql_command) ; 
-    // $sql_command = "drop database TestDb" ; 
-    // $conn->exec($sql_command) ; 
+    // table must have a primary key 
+    $sql_command = "create table Test(
+        id int(6) unsigned auto_increment primary key, 
+        firstname varchar(30) not null,
+        lastname VARCHAR(30) not null,
+        email VARCHAR(50),
+        register_date timestamp default current_timestamp on update current_timestamp
+    )";
+
+    // $sql_command = 'drop table Test' ; 
+
+    $conn->exec($sql_command) ; 
 
     echo "Connected successfully";
-
 } catch (PDOException $e) {
     // echo "Connection failed: " . $e->getMessage();
     echo $sql . "<br>" . $e->getMessage();
 }
 
 
-$conn = null ; 
-
-?>
-
+$conn = null;
