0f042f288269e5a81a6e99baa9aff3effffd64c8
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-08-13 21:11:32 +0600

    targeting existing row and updating it's column values

--- db.php
+++ db.php
@@ -12,42 +12,25 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // $sqlCommand = "select * from Products" ; 
+    // $sqlCommand = "delete from Products where unit_price = '937593'" ; 
 
-    // $sqlCommand = "CREATE TABLE Products (
-    // product_id INT PRIMARY KEY,
-    // product_name VARCHAR(100),
-    // category VARCHAR(50),
-    // unit_price DECIMAL(10, 2)
-    // )";
+    // $sqlCommand = "insert into Products (product_id , product_name , category , unit_price)
+    // values ('8934','text_product' , 'text_caory' , '937593') ";
 
-    // $sqlCommand = "delete from Sales" ;  
+    // update table_name set column1=new_value1 , column2=new_value2 , column3=new_value3
+    // where condition 
 
-    // basic syntex for insert into 
-    // INSERT INTO Table_name (col1,col2,col3,...) VALUES (val1,val2,val3, ...)
 
-    //inserting one row of data 
 
-    // $sqlCommand = "insert into Sales 
-    // (sale_id , product_id , quantity_sold , sale_date , total_price) 
-    //     values 
-    // (88,808, 8,'2024-01-08',8000.00)
-    // ";
 
-    //inserting more then row of data 
+    $sqlCommand = "update Products 
+        set 
+        product_id='8' , product_name='test' , category='test',unit_price='88.8'
+        where unit_price='937593'";
 
-    // $sqlCommand = "INSERT INTO Products (product_id, product_name, category, unit_price) VALUES
-    //     (101, 'Laptop', 'Computer', 500.00),
-    //     (102, 'Smartphone', 'Phone', 300.00),
-    //     (103, 'Headphones', 'Electronics', 30.00),
-    //     (106, 'TV', 'Electronics', 700.00),
-    //      (107, 'AC', 'Electronics', 800.00),
-    //     (104, 'Keyboard', 'Computer', 20.00),
-    //     (105, 'Mouse', 'Computer', 15.00);
-    //     ";
 
-    // $conn->exec($sqlCommand);
-
-    $sqlCommand = "select * from Products" ; 
+    // $sqlCommand = "delete from Products where product_name = 'test'" ; 
 
     $stmt = $conn->prepare($sqlCommand);
     $stmt->execute();
@@ -55,7 +38,7 @@ try {
 
 
     foreach ($products as $product) {
-        echo $product['product_id'] . " | " . $product['product_name'] . " | " . $product['category'] . " | ". "---";
+        echo $product['product_id'] . " | " . $product['product_name'] . " | " . $product['category'] . " | " . "---";
     }
 
     echo "\n";
