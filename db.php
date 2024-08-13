73c7e4d921f02169ba6d6070b81ea46adeaf919a
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-08-13 17:58:18 +0600

    and or not in where statement

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+
+use PDO;
+
 $host = 'localhost';
 $dbName = 'revision';
 $user = "root";
@@ -11,27 +14,28 @@ try {
 
 
 
-    $sqlCommand = "select last_name , email from authors" ; 
+    // $sqlCommand = "select last_name , email from authors" ; 
 
-    // DISTINCT statement 
-    // distinct will return us unique values or no duplicate values 
-    $sqlCommand = "select distinct last_name , email from authors" ;
-    
-    
-    // $sqlCommand = "select count(last_name) as count_author from authors" ; 
+    // syntax for where statement 
+    // select column1 , column2 from table_name where condition 
 
     // echo $conn->exec($sqlCommand ); 
+
+    
+    $sqlCommand = "select first_name , email , birthdate from authors where birthdate > '1995-05-27' and id != 20 and email = 'bogan.lesly@example.com' "  ; 
     
 
+    //simply and or not logic similar to if(condition1 and condition2 or condition3)
+    //if(!condition)
 
     $stmt = $conn->prepare($sqlCommand);
     $stmt->execute();
     $authors = $stmt->fetchAll(); //fetchAll will return us
 
-    echo $authors ; 
+    // echo $authors ; 
 
     foreach($authors as $author){
-        echo $author['last_name']. " | " ; 
+        echo $author['first_name']. " | ". $author['email']. " | " .$author['birthdate']. " | " ; 
     }
 
     echo "\n";
