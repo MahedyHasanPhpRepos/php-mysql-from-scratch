0e1a2a14ed0240ac6496e4f5747ce0247f804e20
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-08-14 12:21:12 +0600

    where in

--- db.php
+++ db.php
@@ -12,60 +12,32 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // where condition search in row 
-    // mysql where column_name like operator search in a column
 
-    // like operator is used in where statement to search for a specific pattern
-    // there is two wildcards used in conjunction the like operator 
-    // 1.The % sign represent zero , one , or multiple characture 
-    // 2.The underscore _ sign represents one single characture 
+    // MySQL IN Operator
+    // The IN operator allows you to specify multiple values in a WHERE clause.
+    // The IN operator is a shorthand for multiple OR conditions.
 
-    // syntex for like operator 
-    // select * from table where column LIKE pattern
+    // SELECT column_name(s)
+    // FROM table_name
+    // WHERE column_name IN (value1, value2, ...);
 
+    // SELECT column_name(s)
+    // FROM table_name
+    // WHERE column_name IN (SELECT STATEMENT);    
 
-    // like 'a%' means find value starts with characture 'a'
-    //  goto product_name column  search start with a then % means other charactures 
-    // $sqlCommand = "select * from Products where product_name like 'a%' ";
-
-    // Finds any values that end with "a"
-    // goto product_name column search with value is end with a and return all row values 
-    // $sqlCommand = "select * from Products where product_name like '%a' ";
+    // The IN operator is a shorthand for multiple OR conditions.
+    // $sqlCommand = "select * from Products where product_name IN ('Laptop','Smartphone')";
+    // $sqlCommand = "select * from Products where product_name NOT IN ('Laptop','Smartphone')";
 
 
-    //  goto product_name column search with value contains 'dp' characture at any position
-    // $sqlCommand = "select * from Products where product_name like '%dp%' ";
-    // $sqlCommand = "select * from Products where product_name like '%a%' ";
 
+    // $subCommand = "select * from Sales where total_price < 60" ;  
 
-    //  goto product_name column search with value which second characture is 'a' and return all row values 
-    //one underscore and a
-    $sqlCommand = "select * from Products where product_name like '_a%' ";
+    // this subquery returning value 
 
-    //  goto product_name column search with value which third characture is 'a' and return all row values 
-    //two underscore and a
-    $sqlCommand = "select * from Products where product_name like '__a%' ";
+    //in operator is working like multiple or and returning value is matched with
+    $sqlCommand = "select * from Products where unit_price in (select total_price from Sales where total_price < 60)";
 
-
-    //  goto product_name column search with value which fourth characture is 'a' and return all row values 
-    //three underscore and a
-    $sqlCommand = "select * from Products where product_name like '___a%' ";
-
-
-    //  goto product_name column search with value which second from back characture is 'a' and return all row values 
-    $sqlCommand = "select * from Products where product_name like '%a_' ";
-
-    //  goto product_name column search with value which second from back characture is 'a' and return all row values 
-    $sqlCommand = "select * from Products where product_name like '%a__' ";
-
-    //  goto product_name column search with value which start with 's' and end with 'e' and return all row values 
-    // $sqlCommand = "select * from Products where product_name like 's%e'";
-
-    // $sqlCommand = "select * from Products where product_name like 'a_%'" ; 
-    //will return ac 
-    
-    // Finds any values that starts with "a" and are at least 3 characters in length
-    $sqlCommand = "select * from Products where product_name like 'a_%_%'" ; 
 
     $stmt = $conn->prepare($sqlCommand);
     $stmt->execute();
@@ -73,7 +45,7 @@ try {
 
 
     foreach ($products as $product) {
-        echo $product['product_id'] . " | " . $product['product_name'] . " | " . $product['category'] . " | " . "---";
+        echo $product['product_id'] . " | " . $product['product_name'] . " | " . $product['category'] . " | " . $product['total_price'] . "---";
     }
 
     echo "\n";
