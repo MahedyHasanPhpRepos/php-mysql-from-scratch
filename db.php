62b23327bc1b5f71a689bc2a4c78c8ab997a7af5
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-08-14 10:31:44 +0600

    where column_name like

--- db.php
+++ db.php
@@ -12,25 +12,60 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // $sqlCommand = "select * from Products" ; 
-    // $sqlCommand = "delete from Products where unit_price = '937593'" ; 
+    // where condition search in row 
+    // mysql where column_name like operator search in a column
 
-    // $sqlCommand = "insert into Products (product_id , product_name , category , unit_price)
-    // values ('8934','text_product' , 'text_caory' , '937593') ";
+    // like operator is used in where statement to search for a specific pattern
+    // there is two wildcards used in conjunction the like operator 
+    // 1.The % sign represent zero , one , or multiple characture 
+    // 2.The underscore _ sign represents one single characture 
 
-    // update table_name set column1=new_value1 , column2=new_value2 , column3=new_value3
-    // where condition 
-
-
-
-
-    $sqlCommand = "update Products 
-        set 
-        product_id='8' , product_name='test' , category='test',unit_price='88.8'
-        where unit_price='937593'";
+    // syntex for like operator 
+    // select * from table where column LIKE pattern
 
 
-    // $sqlCommand = "delete from Products where product_name = 'test'" ; 
+    // like 'a%' means find value starts with characture 'a'
+    //  goto product_name column  search start with a then % means other charactures 
+    // $sqlCommand = "select * from Products where product_name like 'a%' ";
+
+    // Finds any values that end with "a"
+    // goto product_name column search with value is end with a and return all row values 
+    // $sqlCommand = "select * from Products where product_name like '%a' ";
+
+
+    //  goto product_name column search with value contains 'dp' characture at any position
+    // $sqlCommand = "select * from Products where product_name like '%dp%' ";
+    // $sqlCommand = "select * from Products where product_name like '%a%' ";
+
+
+    //  goto product_name column search with value which second characture is 'a' and return all row values 
+    //one underscore and a
+    $sqlCommand = "select * from Products where product_name like '_a%' ";
+
+    //  goto product_name column search with value which third characture is 'a' and return all row values 
+    //two underscore and a
+    $sqlCommand = "select * from Products where product_name like '__a%' ";
+
+
+    //  goto product_name column search with value which fourth characture is 'a' and return all row values 
+    //three underscore and a
+    $sqlCommand = "select * from Products where product_name like '___a%' ";
+
+
+    //  goto product_name column search with value which second from back characture is 'a' and return all row values 
+    $sqlCommand = "select * from Products where product_name like '%a_' ";
+
+    //  goto product_name column search with value which second from back characture is 'a' and return all row values 
+    $sqlCommand = "select * from Products where product_name like '%a__' ";
+
+    //  goto product_name column search with value which start with 's' and end with 'e' and return all row values 
+    // $sqlCommand = "select * from Products where product_name like 's%e'";
+
+    // $sqlCommand = "select * from Products where product_name like 'a_%'" ; 
+    //will return ac 
+    
+    // Finds any values that starts with "a" and are at least 3 characters in length
+    $sqlCommand = "select * from Products where product_name like 'a_%_%'" ; 
 
     $stmt = $conn->prepare($sqlCommand);
     $stmt->execute();
